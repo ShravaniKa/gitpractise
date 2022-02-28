19a987317bcb6092bc5fdd5eca9c25b3542dbeb7
Sam <shravanikasturi40@gmail.com>
2022-02-28 20:52:16 +0530

    american develop commit

---
+++ Log4j/src/gamma.java
@@ -0,0 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import alpha.Demo;
+
+public class gamma {
+	private static Logger log=LogManager.getLogger(Demo.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		log.debug("im in debug mode");
+		log.info("new method");
+	}
+
+}
